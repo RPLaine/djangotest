41bd6e3daf7e5387297e372c5d8c3f5e0a3372f6
RPLaine <riku.laine@gmail.com>
2023-11-07 14:53:48 +0200

    Migrations and views
    Tiny modifications

--- project/registration/views.py
+++ project/registration/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 def registration(request):
     context = {}
+    context['title'] = 'Luo uusi tili'
 
     return render (request, 'registration/registration.html', context)
