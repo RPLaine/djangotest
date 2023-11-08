532c2ad54ce22ad227de1879eb7c0dfe727964d0
RPLaine <riku.laine@gmail.com>
2023-11-08 14:24:38 +0200

    Registration development
    Data route from html to views.py

--- project/registration/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    password1 = forms.PasswordInput()
-    password2 = forms.PasswordInput()
--- project/registration/views.py
+++ project/registration/views.py
@@ -1,7 +1,18 @@
+from webbrowser import get
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def registration(request):
     context = {}
     context['title'] = 'Luo uusi tili'
+
+    if request.method == 'POST':
+        if 'create' in request.POST:
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password1 = request.POST.get('password1')
+            password2 = request.POST.get('password2')
+
+            user = User.objects.create_user(username, email, password1)
 
     return render (request, 'registration/registration.html', context)
